0e812611c4168e6f162d75e28a68f5285ed7755b
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-01-12 19:00:18 -0300

    connecting to supabase DB

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,7 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import Image from "next/image";
+
 export default function Content() {
+  const [filmes, setFilmes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchFilmes = async () => {
+      const { data, error } = await supabase.from("movies").select("*");
+
+      if (error) {
+        console.error(error);
+      } else {
+        setFilmes(data);
+      }
+    };
+    fetchFilmes();
+  });
+
   return (
-    <article>
+    <article className="ml-4 md:ml-0">
       <p className="font-medium text-lg">Recentes</p>
+
+      <ul>
+        {filmes.map((filme) => (
+          <li key={filme.id}>
+            {filme.title} <br /> {filme.synopsis}
+            <img
+              src={filme.link_cover}
+              alt="logo_mcpflix"
+              width={100}
+              height={50}
+            />
+          </li>
+        ))}
+      </ul>
     </article>
   );
 }
