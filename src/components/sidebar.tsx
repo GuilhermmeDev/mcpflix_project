a50b28a7c8d3455ec69ba0a0cfe27453385cb7f2
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-01-16 18:04:01 -0300

    new logo and icon

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,11 +5,11 @@ export default function Sidebar() {
     // tag aside é responsiva e toma diferentes comportamentos dependendo da resolução da tela
     <aside className="md:max-h-full md:sticky md:top-1/4 max-h-24 w-fit min-w-16 md:ml-8 my-4 md:my-8 bg-neutral-800 rounded-3xl p-4 flex md:flex-col items-center sm:w-fit flex-row fixed bottom-2">
       <Image
-        src={"/mcpflix_logo.svg"}
+        src={"/logo_mcpflix.svg"}
         alt="logo_mcpflix"
-        width={100}
-        height={50}
-        className=""
+        width={120}
+        height={100}
+        className="md:my-8 mx-4"
       />
 
       <div className="w-full flex md:flex-col gap-6 flex-row">
