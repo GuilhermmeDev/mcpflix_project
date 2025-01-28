f915331d57353516686f5c35f1991a7eb7f98211
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-01-28 19:06:31 -0300

    giving credits to me

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+
+export default function Footer() {
+  return (
+    <footer className="bg-neutral-900 text-white p-4 flex flex-col items-center absolute bottom-0 w-full">
+      <div className="flex space-x-4 mb-4">
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            <i className="ri-github-fill text-2xl"></i>
+        </a>
+      </div>
+      <p className="text-sm text-gray-400 text-center">© {new Date().getFullYear()} GuilhermmeDev. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
+
