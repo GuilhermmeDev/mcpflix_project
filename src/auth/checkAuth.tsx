798ca91c5bfaea4d3c5bfb2a1ace00c4cd13c7ea
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-07-15 14:52:20 -0300

    fix: logic with redirect route

--- src/auth/checkAuth.tsx
+++ src/auth/checkAuth.tsx
@@ -11,13 +11,14 @@ const useAuth = () => {
   useEffect(() => {
     const checkAuth = async () => {
       const { data } = await supabase.auth.getUser();
+      console.log(data);
 
       // Se o usuário estiver autenticado e em uma rota pública, redirecione para "/"
       if (data.user && publicRoutes.includes(pathname)) {
         router.replace('/dashboard');
       }
       // Se o usuário não estiver autenticado e estiver em uma rota privada, redirecione para a página de login
-      if (!publicRoutes.includes(pathname)) {
+      if (!(data.user || publicRoutes.includes(pathname))) {
         router.replace('/login');
       }
     };
