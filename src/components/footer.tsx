b78c059749e157ebcdc42ba5797fba0185ffc034
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-07-15 18:38:31 -0300

    ui: new footer colors

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="relative bottom-0 mt-6 flex w-full flex-col items-center bg-accent p-4 text-white">
+    <footer className="relative bottom-0 mt-6 flex w-full flex-col items-center border-border border-t bg-sidebar p-4 text-white">
       <div className="mb-4 flex space-x-4 text-muted-foreground">
         <a
           className="hover:text-primary"
